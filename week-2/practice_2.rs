ed7c04137498d282eae449eca38d2342feab4c42
fikun <fredericksoyinka@gmail.com>
2024-12-01 23:26:23 +0100

    (commit)

---
+++ week-2/practice_2.rs
@@ -0,0 +1,6 @@
+fn main() {
+	println!(); // prints just a
+	println!("hello");//prints
+	println!("format {} arguments", "some" ); //prints format some arguments
+	println!("my name is {}. I am the class rep of stream 1", "fikunmi" );
+}
